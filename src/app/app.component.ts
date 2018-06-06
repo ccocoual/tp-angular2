f2dc97d8c9ee60b9e2ed461ac5c475a5db714bfd
Corentin Cocoual <corentin.cocoual@zenika.com>
2018-06-06 19:51:09 +0200

    :sparkles: TP4: Adding stock notion to product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,24 +20,28 @@ export class AppComponent {
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
         photo: 'http://placehold.it/800x500',
         price: 10,
+        stock: 5,
       },
       {
         title: 'Product 2',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
         photo: 'http://placehold.it/800x500',
         price: 20,
+        stock: 1,
       },
       {
         title: 'Product 3',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
         photo: 'http://placehold.it/800x500',
         price: 30,
+        stock: 2,
       },
       {
         title: 'Product 4',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
         photo: 'http://placehold.it/800x500',
         price: 40,
+        stock: 0,
       },
     ];
   }
